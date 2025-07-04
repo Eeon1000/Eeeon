4e4fefeeaef97d7d9f735d8d45ee723173111cdd
Eeon1000 <giyarprana05@gmail.com>
2025-07-05 04:45:41 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,12 @@
-// Firebase config kosong (isi nanti)
+js/firebase-config.js
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "Eeeon.firebaseapp.com",
+  databaseURL: "https://Eeeon.firebaseio.com",
+  projectId: "Eeeon",
+  storageBucket: "Eeeon.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
